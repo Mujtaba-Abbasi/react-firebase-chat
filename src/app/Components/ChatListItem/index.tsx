58338c51787f273ae389ffe3b891aa6c44ef18c0
Mujtaba-Abbasi <gmabbasi92@gmail.com>
2023-07-08 22:52:37 +0500

    Chat Panel, Chat window

--- src/app/Components/ChatListItem/index.tsx
+++ src/app/Components/ChatListItem/index.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+const image =
+  "https://media.istockphoto.com/id/1289220545/photo/beautiful-woman-smiling-with-crossed-arms.jpg?s=612x612&w=0&k=20&c=qmOTkGstKj1qN0zPVWj-n28oRA6_BHQN8uVLIXg0TF8=";
+interface IChatListItemProps {
+  name: string;
+  lastMessage?: string;
+  imageURL: string;
+  onSelect?: () => void;
+}
 
-const ChatListItem = () => {
+const ChatListItem = ({
+  name,
+  lastMessage,
+  imageURL,
+  onSelect,
+}: IChatListItemProps) => {
   return (
-    <div className="user-chat">
-      <img
-        src="https://media.istockphoto.com/id/1289220545/photo/beautiful-woman-smiling-with-crossed-arms.jpg?s=612x612&w=0&k=20&c=qmOTkGstKj1qN0zPVWj-n28oRA6_BHQN8uVLIXg0TF8="
-        alt=""
-      />
+    <div className="user-chat" onClick={onSelect}>
+      <img src={imageURL ?? image} alt="" />
       <div className="user-chat-info">
-        <span>Person Name</span>
-        <p>Last Message</p>
+        <span>{name}</span>
+        {/* {lastMessage && <p>{lastMessage}</p>} */}
       </div>
     </div>
   );
--- src/app/Components/Input/index.tsx
+++ src/app/Components/Input/index.tsx
@@ -1,18 +1,58 @@
-import React from "react";
-import img from "/images/img.png";
+import { FormEvent, useContext, useEffect, useState } from "react";
+import placeholderImage from "/images/img.png";
+import { useMessages } from "../../hooks/useMessages";
+import { ChatContext } from "../../context/ChatContext";
 
 const Input = () => {
+  const {
+    state: { chatId },
+  } = useContext(ChatContext);
+  const [text, setText] = useState<string>("");
+  const [image, setImage] = useState<File | null>(null);
+  const { onMessageSend } = useMessages();
+  const handleSendMessage = async (e: FormEvent) => {
+    e.preventDefault();
+    await onMessageSend(text, image, () => {
+      setText("");
+      setImage(null);
+    });
+  };
+
   return (
-    <div className="input">
-      <input type="text" placeholder="Type some message here..." />
-      <div className="send">
-        <input type="file" style={{ display: "none" }} id="file" />
-        <label htmlFor="file">
-          <img alt="" src={img} />
-        </label>
-        <button>Send</button>
+    <form action="submit" onSubmit={(e) => handleSendMessage(e)}>
+      <div className="input">
+        <input
+          type="text"
+          placeholder={
+            chatId
+              ? "Type some message here..."
+              : "Select a chat to start messaging"
+          }
+          value={text}
+          disabled={!chatId}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="send">
+          <input
+            type="file"
+            style={{ display: "none" }}
+            id="file"
+            onChange={(e) => {
+              setImage(e.target.files?.[0] ?? null);
+            }}
+            disabled={!chatId}
+          />
+          <label htmlFor="file">
+            <img
+              alt=""
+              src={(image && URL.createObjectURL(image)) ?? placeholderImage}
+              style={{ borderRadius: "4px", height: "35px", width: "35px" }}
+            />
+          </label>
+          <button disabled={text.length === 0 && image === null}>Send</button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
