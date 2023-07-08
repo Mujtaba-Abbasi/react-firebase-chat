92bf6f96a48af4657db53878d2c13ad4283ca539
Mujtaba-Abbasi <gmabbasi92@gmail.com>
2023-07-08 22:52:16 +0500

    Messages

--- src/app/Components/Message/index.tsx
+++ src/app/Components/Message/index.tsx
@@ -1,13 +1,43 @@
-const Message = () => {
+import { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { ChatContext } from "../../context/ChatContext";
+import { useMessages } from "../../hooks/useMessages";
+
+interface IMessage {
+  message: any;
+}
+
+const Message = ({ message }: IMessage) => {
+  const currentUser = useContext(AuthContext);
+  const {
+    state: { user },
+  } = useContext(ChatContext);
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [message]);
+
   return (
-    <div className="message owner">
+    <div
+      className={`message ${currentUser.uid === message?.senderId && "owner"}`}
+      ref={containerRef}
+    >
       <div className="messageInfo ">
-        <img src="https://avatars.githubusercontent.com/u/7?v=4" alt="avatar" />
+        <img
+          src={
+            currentUser.uid === message?.senderId
+              ? currentUser.photo ?? ""
+              : user.photoURL ?? "https://avatars.githubusercontent.com/u/7?v=4"
+          }
+          alt="avatar"
+        />
         <span className="message-time">Just Now</span>
       </div>
       <div className="messageContent">
-        <p>hellow</p>
-        <img src="https://avatars.githubusercontent.com/u/7?v=4" alt="avatar" />
+        {message.text && <p>{message.text}</p>}
+        {message.imageUrl && <img src={message.imageUrl} alt="avatar" />}
       </div>
     </div>
   );
